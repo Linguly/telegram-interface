67d3643c34e55b01b16b57bd174853cb38dfcb70
Hadi <65487316+neo2100@users.noreply.github.com>
2025-06-12 23:14:30 +0200

    Add agent description (#14)

--- src/server/scenes/util/messenger.ts
+++ src/server/scenes/util/messenger.ts
@@ -1,7 +1,7 @@
 import { Context } from 'telegraf';
 import { removeKeyboard } from './sceneCommon';
 
-const reply = async (ctx: Context, message: string, options?: any) => {
+const reply = async (ctx: Context, message: string, options?: any, delay: number = 600) => {
     if (options === undefined)
         options = removeKeyboard;
     options.parse_mode = 'Markdown';
@@ -9,9 +9,9 @@ const reply = async (ctx: Context, message: string, options?: any) => {
     /* pretend the bot is typing */
     await ctx.sendChatAction('typing');
     /* delay between replies */
-    await new Promise(resolve => setTimeout(resolve, 600));
+    await new Promise(resolve => setTimeout(resolve, delay));
     /* send the message */
     await ctx.reply(message, options);
-}
+};
 
 export { reply };
