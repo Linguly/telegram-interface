3eaa91ce542166c4a00995d9ded4b74739b5b555
Hadi <65487316+neo2100@users.noreply.github.com>
2025-06-29 18:50:14 +0200

    20 masking agent to learn words and phrases (#17)
    * Add start the Agent
    
    * Update the return type
    
    * minor clean up

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,4 @@ dotenv.config();
 import botServer from './server/index';
 botServer(); /* start the bot */
 
-
-//import './server/logic/redisUpdaterServer/index'; /* start the redis DB */
-
 console.log(`Expecting Linguly Core API to be running at ${process.env.LINGULY_CORE_BASE_URL}`);
--- src/server/services/agents.ts
+++ src/server/services/agents.ts
@@ -35,6 +35,18 @@ export async function getAgentsFiltered(ctx: LingulyContext, categories: string[
     return response;
 }
 
+export async function startTheAgent(ctx: LingulyContext, agentId: string) {
+    var userToken = await getUserToken(ctx);
+    if (!userToken) {
+        return { success: false, message: 'User not authenticated', status: 401 };
+    }
+    const method = 'POST';
+    const url = `${process.env.LINGULY_CORE_BASE_URL}/agents/${agentId}/start`;
+
+    const response = await callLingulyCoreApi(method, url, {}, userToken);
+    return response;
+}
+
 export async function chatWithAgent(ctx: LingulyContext, agentId: string, content: string) {
     var userToken = await getUserToken(ctx);
     if (!userToken) {
@@ -42,7 +54,7 @@ export async function chatWithAgent(ctx: LingulyContext, agentId: string, conten
     }
     const method = 'POST';
     const url = `${process.env.LINGULY_CORE_BASE_URL}/agents/${agentId}/chat`;
-    const body = { content };
+    const body = [{ content, role: 'user' }];
 
     const response = await callLingulyCoreApi(method, url, body, userToken);
     return response;
